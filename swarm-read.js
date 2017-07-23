99a3894f0c679a1b65ef8e68103bc31c5afb5ba5
cblgh <cblgh@cblgh.org>
2017-07-23 20:47:20 +0200

    init

--- swarm-read.js
+++ swarm-read.js
@@ -5,7 +5,11 @@ var hyperdiscovery = require("hyperdiscovery")
 var ram = require("random-access-memory")
 
 // pass in the key that's printed from swarm-write.js
-var remote = process.argv[2] || "003afb49a31f4f6a7a487c1439ce2bc3eedc622450f602ef783d37051770251f"
+var remote = process.argv[2]
+if (!remote) {
+    console.log("usage: node swarm-read.js <key from swarm-write.js|other hypercore key>")
+    process.exit()
+}
 // we pass the ram as storage to skip creating (and consequently cleaning up after) lots of folders
 var feed = hypercore(ram, remote, {valueEncoding: "json"})
 
@@ -22,6 +26,8 @@ feed.on("ready", function() {
 })
 
 // create a readStream so that we can log all of the data we get from the peers we connect with
+// 'start: 0' makes the stream start at the beginning of the log
+// 'live: true' keeps the stream open after the last bit of data has been read, yielding more data as it comes in
 var stream = feed.createReadStream({start: 0, live: true})
 stream.on("data", function(data) {
     console.log("data", data)
